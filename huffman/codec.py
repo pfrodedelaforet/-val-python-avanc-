8ead70810c785a99473a757021409036fd17c723
pfrodedelaforet <pfrodedelaforet@users.noreply.github.com>
2020-01-14 16:56:11 +0100

    fejjejejeje

--- huffman/codec.py
+++ huffman/codec.py
@@ -1,67 +1,52 @@
 import numpy as np
 class Node : 
-	
-    def __init__(self, label = None, right_nb = None, left_nb = None, cardi = 0, linked = True) :
+    """ on créé la classe Node, qu'on utilisera ensuite recursivement pour créer la classe BinaryTree"""
+    def __init__(self, label = None, right_nb = None, left_nb = None, cardi = 0) :
         self.right_nb = right_nb
         self.left_nb  = left_nb
         self.label = label
         self.cardi = cardi
-        self.linked = linked
     def __eq__(self, node2):
         return self.label == node2.label
-    @property 
-    def linked(self):
-        return self._linked
-    @linked.setter
-    def linked(self, other):
-        if type(other) != bool : 
-            raise ValueError('un booleen imbecile')
-        self._linked = other 
     def __repr__(self):
         return f"{self.label}, {self.cardi}"
 class BinaryTree:
-	"""Docstring"""
-	def __init__(self, root_node = None, ends = list()):
-		self.root = root_node
-		self.ends = ends
-
-	def find_way(self, label):
-		node = self.root
-		way = ''
-		while node.label != label:#tant qu'on n'est pas arrivé à label (on va de racine à bout)
-			if label in node.right_neigh.label:
-				node = node.right_neigh
-				way += '1'
-			else:
-				node = node.left_neigh
-				way += '0'
-		return way
-
-
-"""        
-sort(L : list, coefs : dict):
-    on choisit une adaptation du tri par insertion car les listes 
-    ne peuvent pas être très longues (au maximum autant que de lettres 
-    de l'alphabet, de nombres, et de ponctuation), la complexité de ce programme a donc peu d'importance
-    for i in range(len(L)):
-        x = L[i]
-        j = i
-        while j > 0 and coefs[L[j-1]] > coefs[L[i]] : 
-            L[j] = L[j-1] 
-            coefs[L[j]] = coefs[L[j-1]]
-            j -= 1
-            
-        L[j] = x
-
-"""    
-
+    """Cette classe BinaryTree permettra de créer l'arbre qui codera le message
+     uniquement en mettant en entrée le noeud racine et les noeuds de bout, et de pouvoir
+     coder en binaire un chemin entre deux noeuds"""
+    def __init__(self, root_node = None, ends = list()):
+        self.root = root_node
+        self.ends = ends
+    
+    def find_way(self, label):
+        node = self.root
+        way = ''
+        while node.label != label:#tant qu'on n'est pas arrivé à label (on va de racine à bout)
+            if label in node.right_nb.label:
+                node = node.right_nb
+                way += '1'
+            else:
+                node = node.left_nb
+                way += '0'
+        return way                
+    def find_char(self, bincode):
+        node = self.root
+        for x in bincode : 
+            if x == '1' : 
+                node = node.right_nb
+            if x == '0' : 
+                node = node.left_nb
+        return node.label
 
 
 class TreeBuilder : 
     def __init__(self, text):
         self.text = text
     def tree(self):
-        """ truc"""
+        """ l'idée ici, c'est de partir des noeuds du bout, qui sont les caractères isolés,
+        d'initier une liste de ces noeuds, rangés par cardinal décroissant, et de les fusionner 2 à 2
+        par ordre croissant de cardinal. On garde ainsi à chaque itération une liste triée des noeuds qu'il reste
+        à fusionner, et on s'arête lorsqu'il ne reste qu'un noeud"""
         text = self.text
         ends = list(set(text)) #il s'agit de la lise de tous les caractères distincts
         def card(character):
@@ -72,11 +57,9 @@ class TreeBuilder :
             return i
         ends.sort(key = card)
         ends.reverse()
-        nodeslist = [Node(x, None, None, card(x), False) for x in ends]
+        nodeslist = [Node(x, None, None, card(x)) for x in ends]
         node_ends = np.copy(nodeslist)
-        old_card = nodeslist[-1].cardi
-        while len(nodeslist[0].label) < len(ends) : 
-            j = len(nodeslist) - 1
+        while len(nodeslist) > 1 : 
             print([x.label for x in nodeslist])
             x = nodeslist.pop()
             y = nodeslist.pop()
@@ -85,19 +68,36 @@ class TreeBuilder :
             i = len(nodeslist) - 1 
             while i > 0 and nodeslist[i].cardi > nodecardi: 
                 i -= 1
-            nodeslist.insert(i, Node(nodelabel, x.label, y.label,  nodecardi))
+            nodeslist.insert(i, Node(nodelabel, x, y,  nodecardi))
         print(nodeslist)
         return BinaryTree(nodeslist[0],  node_ends)
 
+class Codec : 
+    def __init__(self, tree : BinaryTree):
+        self.tree = tree 
+    def encode(self, text) : 
+        arbre = self.tree
+        binarychain = ''
+        for s in text : 
+            binarychain += arbre.find_way(s)
+        return binarychain
+    def decode(self, encoded):
+        chaine = ''
+        arbre = self.tree
+        bin_provis = ''
+        for x in encoded : 
+            bin_provis += x
+            char = arbre.find_char(bin_provis)
+            if len(char) == 1 :
+                chaine += char
+                bin_provis = ''
+        return chaine
 
-
-tb = TreeBuilder('nique ta daronne')
-tb.tree()
-
-
-chaine = "abc"
-L = ["x", "y", "z", "a"]
-def cle(x):
-    return 1 if x == "a" else 0
-L.sort(key = cle)
-print(L)
+texte = "a dead dad ceded a bad babe a beaded abaca bed"           
+builder = TreeBuilder("a dead dad ceded a bad babe a beaded abaca bed")
+tr = builder.tree()
+codec = Codec(tr)
+print(codec.encode("a dead dad ceded a bad babe a beaded abaca bed"))
+print(codec.decode(codec.encode("a dead dad ceded a bad babe a beaded abaca bed")))
+decoded = codec.decode(codec.encode("a dead dad ceded a bad babe a beaded abaca bed"))
+print(texte == decoded)
